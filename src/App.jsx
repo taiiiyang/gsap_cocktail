a2c243f72d18db326478fa3d9d880d85242d0abb
taiiiyang <781547101@qq.com>
2025-07-04 20:12:20 +0800

    feat: initial navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "@/components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center w-screen h-screen'>
-      <h1 className='text-3xl font-bold'>Hello World</h1>
-    </div>
+    <main className='w-screen h-screen'>
+      <Navbar></Navbar>
+    </main>
   );
 };
 
