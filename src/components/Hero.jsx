84ece0d42329fd76f445dc4dd4f20c8c92178038
taiiiyang <781547101@qq.com>
2025-07-06 17:54:00 +0800

    feat: implement hero layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,6 +41,25 @@ function Hero() {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata(() => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    });
   }, []);
 
   return (
@@ -73,9 +92,9 @@ function Hero() {
         </div>
       </section>
 
-      {/* <div className='video absolute inset-0'>
+      <div className='video absolute inset-0'>
         <video ref={videoRef} muted playsInline preload='auto' src='/videos/output.mp4' />
-      </div> */}
+      </div>
     </>
   );
 }
